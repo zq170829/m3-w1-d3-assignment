ce919d4ce651a21c88f1ee5e679cad9447376590
Qi Zhang <zq170829@gmail.com>
2021-10-30 21:24:17 -0500

    m3-w2-d3-assignment

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+
+router.get("/", function (req, res) {
+  // res.send('It works!');
+  res.render("form", { title: "Registration form" });
+});
+
+router.post(
+  "/",
+  [
+    check("name").isLength({ min: 1 }).withMessage("Please enter a name"),
+    check("email").isLength({ min: 1 }).withMessage("Please enter an email"),
+  ],
+  function (req, res) {
+    //console.log(req.body);
+    if (errors.isEmpty()) {
+      res.send("Thank you for your registration!");
+    } else {
+      res.render("form", {
+        title: "Registration form",
+        errors: errors.array(),
+        data: req.body,
+      });
+    }
+  }
+);
+
+module.exports = router;
